1598a9136a4da4c6bcb62699c57730a7ba7f3c08
muhammed jasim k <muhammedjasim1000@gmail.com>
2023-08-13 11:58:28 +0530

    mini-project

---
+++ script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.nav-items');
+// let navdiv = document.querySelector('.navbars');
+
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('bi-x')
+    navbar.classList.toggle('navbar-active')
+
+})
+navbar.addEventListener('click',function(){
+    menu.classList.remove('bi-x')
+    navbar.classList.remove('navbar-active')
+
+})
+  
